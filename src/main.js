1d88619917ec874dc916a8983a5d5c7c1aed0e9c
Michalskii <94405347+Michalskii@users.noreply.github.com>
2022-07-27 14:33:15 +0200

    d

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import './index.css';
 // require('@/assets/main.scss');
-import './../node_modules/Bulma/CSS/bulma.css';
+import '@/../node_modules/Bulma/CSS/bulma.css';
 import App from './App.vue';
 import router from './router';
 
