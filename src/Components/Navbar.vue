71660451ca5802c302cd4413a6314b4216f74811
Michalskii <94405347+Michalskii@users.noreply.github.com>
2022-08-02 16:45:32 +0200

    d

--- src/Components/Navbar.vue
+++ src/Components/Navbar.vue
@@ -1,21 +1,5 @@
  <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io"> Chatty </a>
-
-      <a
-        role="button"
-        class="navbar-burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
     <div id="navbarBasicExample" class="navbar-menu">
       <div class="navbar-start text-xl">
         <RouterLink class="navbar-item" to="/">Home</RouterLink>
